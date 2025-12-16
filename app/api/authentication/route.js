84a79a8cb372b1b546c1430f3670f5565b747b19
Greggory Wiley <admin@tlchatt.com>
2025-12-16 09:49:50 -0500

    st

---
+++ app/api/authentication/route.js
@@ -0,0 +1,81 @@
+import { auth } from "@/lib/auth"; // path to your auth file
+import { headers } from "next/headers"
+export async function POST(request) {
+    try {
+        const json = await request.json()
+        console.log('signInEmail json', json)
+        /*
+        let data = await auth.api.signUpEmail({//https://www.better-auth.com/docs/authentication/email-password#sign-up
+            body: {
+                name: json.username, // required
+                email: json.username, // required
+                password: json.password, // required
+                //image: "https://example.com/image.png",
+                // callbackURL: "https://example.com/callback",
+            },
+        })
+            */
+        const data = await auth.api.signInEmail({
+            body: {
+                email: json.username, // required
+                password: json.password, // required
+                rememberMe: true,
+                //callbackURL: "https://example.com/callback",
+            },
+            // This endpoint requires session cookies.
+            headers: await headers(),
+        });
+        console.log('signIn data', data)
+        /*
+        let session = await auth.api.getSession({
+            headers: await headers()
+        })
+        console.log('session data', session)
+        if (!session) {
+            console.log('!session')
+        }
+            */
+        return Response.json(data)
+
+    }
+    catch (error) {
+        return new Response(`Webhook error: ${error.message}`, {
+
+            status: 400,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            },
+        })
+    }
+    /*
+    return new Response('Success!', {
+        status: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
+        body: {
+            "jwtAuthorizationToken": "eyJhbGciOiJIUzUxMiJ9.eyJmbmFtZSI6Ikx1a2UiLCJzdWIiOiJsdWtlc2VhcnJhQGljbG91ZC5jb20iLCJsbmFtZSI6IiIsInR6IjoiQW1lcmljYS9Ub3JvbnRvIiwidGFnaWRzIjpbMTAyLDEyMiwyMjQsMjI2LDIyOCwzMzAsNDQ2LDYxMiw2MTYsNjIwLDYzMiw2OTgsNzg4LDEwMzYsMTMwMV0sImV4cCI6MTc2NTkxMjAxNiwiaWF0IjoxNzY1ODI1NjE2LCJjaWQiOjQxMTg0N30.JLW9ezWmdkQX71VFGT2WOw5Eu1ucx1YSn6ePiRy84oTUhIpdVLJ27d37fBwtBZeKaHyR5LHOvcb7MEqPRDGoNw",
+            "jwtRefreshToken": "eyJhbGciOiJIUzUxMiJ9.eyJhbGxhY2Nlc3MiOnRydWUsInN1YiI6Imx1a2VzZWFycmFAaWNsb3VkLmNvbSIsInR6IjoiQW1lcmljYS9Ub3JvbnRvIiwiZnJlZW1lbSI6dHJ1ZSwidHlwZSI6InJlZnJlc2giLCJleHAiOjE3ODEzNzc2MTYsInNwIjp0cnVlLCJpYXQiOjE3NjU4MjU2MTYsImNpZCI6NDExODQ3fQ.Lpdq06b0wowjiV4WeYV9s0TCgtrPMGYn7hRgbxQKil4oh_P2MxSDk80hchDJEaUo6bUNQaVY928u-ntNeUcapQ",
+            "timezone": "America/Toronto",
+            "isAllAccessUser": true,
+            "isFreeMember": true,
+            "hasCourseProduct": true
+        }
+    })
+    */
+}
+// GET just to return 200 status for preflight to work
+export async function GET() {
+    return new Response('Success!', {
+        status: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
+    })
+}
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,45 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+    crossOrigin: 'anonymous',
+    async headers() {
+    return [
+      {
+        source: "/api/authentication",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+            {
+        source: "/api/authentication/session",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
