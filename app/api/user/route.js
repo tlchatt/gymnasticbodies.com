02269097f40d27ef692b23a96f039daf49507497
Prachi Chaudhary <admin@tlchatt.com>
2025-12-23 02:15:49 +0530

    login form updates

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,16 +1,30 @@
 import { db } from "@/Drizzle/index.ts"; // your drizzle instance
-import { user_setting, user } from "@/Drizzle/db/schema"
+import { user_setting, user, account } from "@/Drizzle/db/schema"
 import { eq } from 'drizzle-orm';
 import { v4 as uuidv4 } from 'uuid';
-
+import { auth } from "@/lib/auth"; // path to your auth file
+import { headers } from "next/headers"
+import generatePassword from 'generate-password';
+import { sendCodeEmailSG, sendCredentialsEmailSG, sendResetLinkEmailSG } from "@/lib/sendgrid";
 
 export async function POST(request) {
     const contentType = request.headers.get('Content-Type');
-    console.log('Content-Type:', contentType);
+    const password = generatePassword.generate({//https://www.npmjs.com/package/generate-password
+        length: 10,//for better auth 8 is min characters required
+        numbers: true,
+        symbols: true,
+        strict: true
+    });
 
+    console.log("password is:", password)
+    console.log('Content-Type:', contentType);
     const json = await request.json()
+    let newUser = true
     console.log('user json', json)
-    
+    console.log('user confirmPassword', json.confirmPassword)
+    console.log('user userId', json.userId)
+
+
     let testJsonForSubscription = {
         status: "active",
         next_payment_date_gmt: "2025-12-26T17:22:53",
@@ -20,33 +34,55 @@ export async function POST(request) {
             email: 'greggorywiley@tlchatt.com'
         }
     }
-    //status : on-hold / active / cancelled
-    if (json.status) {
-        //write settings, user_setting table
-        let subscriptionData = {
-            status: json.status,
-            renewaldate: json.next_payment_date_gmt,
-            startdate: json.start_date_gmt
+
+
+    if (json.email && !(json.status)) {//email to get the reset link
+        //check if email in incoming data exists in user table, if it does, skip account creation and directly send email about the link to reset password
+        let userExist = await doesUserExist()
+        console.log("userExist is:", userExist?.userInfo)
+
+        if (userExist.status) {//send the reset link email
+            newUser = false
+            let emailSent = await sendEmail(newUser, userExist)
+            if (emailSent) {
+                return new Response('OK', { status: 200 });
+            }
+        } else {
+            //do registration first -> send unsuccessful status. 
+            return new Response(`New user in password reset, so error: ${error.message}`, {
+
+                status: 400,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                },
+            })
         }
-        console.log('subscriptionData data:', subscriptionData);
+    }
 
-        const newUser = await db.insert(user).values({
-            id: uuidv4(),
-            name: json.billing.first_name,
-            email: json.billing.email,
-            emailVerified: 'false'
-        }).returning({ id: user.id });
-        console.log("newUser:", newUser[0].id)
+    if (json.confirmPassword && json.userId) {//reset incoming password to users password
+        let updateCredential = await updateUserCredentials()
+        console.log("updateCredential is:", updateCredential)
+        if (updateCredential) {
+            return new Response('OK', { status: 200 });
+        }
 
-        const attachSetting = await db.insert(user_setting).values({
-            type: 'subscription',
-            data: subscriptionData,
-            userId: newUser[0].id
-        }).returning();
+    }
 
-        console.log("attachSetting:", attachSetting)
+    if (json.status) {//when subscription webhook is triggered -> status : on-hold / active / cancelled
+
+        let newAccountInfo = await createAccountForUser()
+
+        let accountSetting = await createAccountSettingsForUser(newAccountInfo)
+
+        if (accountSetting) {
+            await sendEmail(newUser)
+        }
 
         return new Response('OK', { status: 200 });
+
+
     } else {
         try {
             let queryExisting = await db.select().from(user_setting).where(eq(user_setting.userId, json.userId));
@@ -90,6 +126,81 @@ export async function POST(request) {
         }
     }
 
+    async function doesUserExist() {
+        let userExistsQuery = await db
+            .select()
+            .from(user)
+            .where(eq(user.email, json.email));
+        console.log("userExistsQuery is:", userExistsQuery)
+        if (userExistsQuery.length > 0) {
+            return { status: true, userInfo: userExistsQuery[0] }
+        } else {
+            return { status: false }
+        }
+
+    }
+    async function updateUserCredentials() {
+        let updateQuery = await db.update(account)
+            .set(
+                {
+                    password: json.confirmPassword,
+                }
+            ).where(eq(account.userId, json.userId)).returning();
+        console.log("updateQuery is:", updateQuery)
+        return updateQuery
+    }
+    async function createAccountForUser() {
+        //create account field, which will create the user too.
+        const signUpData = await auth.api.signUpEmail({
+            body: {
+                name: json.billing.first_name, // required
+                email: json.billing.email, // required
+                password: password, // required
+            },
+        });
+        console.log("signUpData is:", signUpData)
+        return signUpData
+    }
+    async function createAccountSettingsForUser(newAccountInfo) {
+        //write settings, for the user_setting table
+        let subscriptionData = {
+            status: json.status,
+            renewaldate: json.next_payment_date_gmt,
+            startdate: json.start_date_gmt
+        }
+
+        //attach the new user to user_setting table with subscription data
+        const attachedSettingInfo = await db.insert(user_setting).values({
+            type: 'subscription',
+            data: subscriptionData,
+            userId: newAccountInfo.user.id
+        }).returning();
+        console.log("attachSetting:", attachedSettingInfo)
+        return attachedSettingInfo;
+    }
+    async function sendEmail(newUser, userExist) {
+        let data = {}
+        let emailSent
+        if (newUser) {
+            data = {
+                email: json.billing.email,
+                password: password
+            }
+            emailSent = await sendCredentialsEmailSG(data)
+        } else {
+            data = {
+                email: json.email,
+                userId: userExist.userInfo.id
+            }
+            emailSent = await sendResetLinkEmailSG(data)
+        }
+
+        console.log("email data is:", data)
+
+        console.log("emailSent:", emailSent)
+        return emailSent;
+
+    }
 
     /*
     return new Response('Success!', {
