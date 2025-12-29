3838897d1dccb1fdccf04cdeb2f445ef5ffa6fef
Prachi Chaudhary <admin@tlchatt.com>
2025-12-29 23:03:08 +0530

    subscription route updated

---
+++ tmp/subscriptionRenewed.js
@@ -0,0 +1,132 @@
+json = {
+    id: 426092,
+    parent_id: 426091,
+    status: 'active',
+    currency: 'USD',
+    version: '6.4.1',
+    prices_include_tax: false,
+    date_created: '2024-12-22T08:00:35',
+    date_modified: '2025-12-29T08:01:46',
+    discount_total: '0.00',
+    discount_tax: '0.00',
+    shipping_total: '0.00',
+    shipping_tax: '0.00',
+    cart_tax: '0.00',
+    total: '179.88',
+    total_tax: '0.00',
+    customer_id: 33502,
+    order_key: 'wc_order_sqFnqUVV7FX9W',
+    billing: {
+      first_name: 'CLIFFORD HUTCHISON',
+      last_name: '',
+      company: '',
+      address_1: '',
+      address_2: '',
+      city: '',
+      state: '',
+      postcode: '76016',
+      country: 'US',
+      email: 'butchdds@gmail.com',
+      phone: ''
+    },
+    shipping: {
+      first_name: '',
+      last_name: '',
+      company: '',
+      address_1: '',
+      address_2: '',
+      city: '',
+      state: '',
+      postcode: '',
+      country: '',
+      phone: ''
+    },
+    payment_method: 'authorize_net_cim_credit_card',
+    payment_method_title: 'Auth Credit Card',
+    customer_ip_address: '69.237.16.48',
+    customer_user_agent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+    created_via: 'checkout',
+    customer_note: '',
+    date_completed: null,
+    date_paid: '2025-12-29T08:01:45',
+    number: 'GB426092',
+    meta_data: [
+      {
+        id: 19064847,
+        key: '_billing_wooccm11',
+        value: 'butchdds@gmail.com'
+      },
+      { id: 19064848, key: 'is_vat_exempt', value: 'no' },
+      {
+        id: 19064849,
+        key: '_mbr_woo_infusionsoft_invoice_id',
+        value: '265367'
+      },
+      {
+        id: 19064850,
+        key: '_mbr_infusionsoft_order_pending',
+        value: '1'
+      },
+      {
+        id: 19064851,
+        key: '_mbr_infusionsoft_order_id',
+        value: '265367'
+      },
+      {
+        id: 19064878,
+        key: '_wc_authorize_net_cim_credit_card_payment_token',
+        value: '717049186'
+      },
+      {
+        id: 19064879,
+        key: '_wc_authorize_net_cim_credit_card_customer_id',
+        value: '711292007'
+      }
+    ],
+    line_items: [
+      {
+        id: 468932,
+        name: 'GymFit TV - $239.88 USD / Year',
+        product_id: 51,
+        variation_id: 56,
+        quantity: 1,
+        tax_class: '',
+        subtotal: '179.88',
+        subtotal_tax: '0.00',
+        total: '179.88',
+        total_tax: '0.00',
+        taxes: [],
+        meta_data: [Array],
+        sku: 'gbannual',
+        price: 179.88,
+        parent_name: 'GymFit TV'
+      }
+    ],
+    tax_lines: [],
+    shipping_lines: [],
+    fee_lines: [],
+    coupon_lines: [],
+    payment_url: 'https://www.gymnasticbodies.com/checkout/order-pay/426092/?pay_for_order=true&key=wc_order_sqFnqUVV7FX9W',
+    date_created_gmt: '2024-12-22T15:00:35',
+    date_modified_gmt: '2025-12-29T15:01:46',
+    date_completed_gmt: null,
+    date_paid_gmt: '2025-12-29T15:01:45',
+    billing_period: 'year',
+    billing_interval: '1',
+    start_date_gmt: '2024-12-22T15:00:32',
+    trial_end_date_gmt: '2024-12-29T15:00:32',
+    next_payment_date_gmt: '2026-12-29T15:01:45',
+    payment_retry_date_gmt: '',
+    last_payment_date_gmt: '2025-12-29T15:01:44',
+    cancelled_date_gmt: '',
+    end_date_gmt: '',
+    resubscribed_from: '',
+    resubscribed_subscription: '',
+    removed_line_items: [],
+    _links: {
+      self: [ [Object] ],
+      collection: [ [Object] ],
+      customer: [ [Object] ],
+      up: [ [Object] ]
+    }
+  }
