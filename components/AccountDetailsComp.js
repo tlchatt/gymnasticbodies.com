62c3b70bb6d47c26b6020144b6f9d3329cba9d4c
Prachi Chaudhary <admin@tlchatt.com>
2026-04-28 11:16:37 +0530

    modalpopup update and account detail page updates

--- components/AccountDetailsComp.js
+++ components/AccountDetailsComp.js
@@ -5,15 +5,17 @@ import Typography from '@mui/material/Typography';
 import { GetSettings } from "@/lib/GetSettings.js";
 import { DateTime } from 'luxon';
 import { StandardContainer } from '@/components/StandardContainer/StandardContainer';
-import { getUserWithEmail, queryUserSetting } from '@/lib/userSettings';
+import { getUserWithEmail, queryUserSetting, updateUserSetting } from '@/lib/userSettings';
 import { user } from "@/app/context/stateContext";
 import useSWR from 'swr'
 import axios from "axios";
 import CircularIndeterminate from '@/components/CircularLoading';
 import { Button, Stack } from '@mui/material';
+import ModalPopUp from './ModalPopUp';
+import { CancelSubscriptionInAuthorize } from '@/lib/commonServerFunction';
 import { useState } from 'react';
 export default function AccountDetailsComp(props) {
-    // let { customerId, setCustomerId } = user()
+    let [cancelled, setCancelled] = useState(false)
     /*//customer profile id
     //555933485 - for transaction history
     //803450130 - for subscription history
@@ -25,7 +27,7 @@ export default function AccountDetailsComp(props) {
 
     */
     let appUrl = process.env.NEXT_PUBLIC_API_URL
-    console.log("appUrl:",appUrl)
+    console.log("appUrl:", appUrl)
     // const token = searchParams.get('token');
     // const userId = searchParams.get('userId');
     let userInfoFetch = (url) => fetch(url, {
@@ -39,7 +41,8 @@ export default function AccountDetailsComp(props) {
             type: 'subscription'
         })
     }).then((res) => res.json())
-    const { data, error, isLoading } = useSWR(`https://gymnasticbodies-com.vercel.app/api/user/accountInformation`, userInfoFetch)
+    //https://gymnasticbodies-com.vercel.app
+    const { data, error, isLoading } = useSWR(`${appUrl}/api/user/accountInformation`, userInfoFetch)
     // console.log("data in useSWR is:", data)
     // console.log("error in useSWR is:", error)
     // console.log("isLoading in useSWR is:", isLoading)
@@ -113,6 +116,62 @@ export default function AccountDetailsComp(props) {
                 });
         }
     }*/
+    const cancelSubscription = async () => {
+        console.log("clicked", impInfo?.subscriptionId)
+        let subscription = await CancelSubscriptionInAuthorize(impInfo?.subscriptionId)
+        console.log("subscription:", subscription)
+        let settings
+        if (subscription?.status) {
+            //update neon db 
+            //get neon db user
+            let matching = await queryUserSetting(impInfo?.id, 'subscription')
+            if (matching) {
+                let settingsRecord = { ...matching }
+
+                let updatedData = JSON.parse(matching.data)
+                updatedData.status = 'Inactive'
+                updatedData.renewaldate = 'N/A'
+
+                settingsRecord.status = 'inactive'
+                settingsRecord.data = JSON.stringify(updatedData)
+                settings = await updateUserSetting(matching, settingsRecord)
+                console.log("settings after updateUserSetting:", settings)
+                console.log("settings[0]?.status:", settings[0]?.status)
+                if (settings[0]?.status == "Inactive" || settings[0]?.status == "inactive") {
+                    setCancelled(true)
+                }
+            }
+        }
+    }
+    let cancelSubscriptionData = [
+        {
+            title: "Are you sure you want to cancel?",
+            subTitle: `If you cancel now, you will lose your access after ${impInfo?.redableNextPaymentDate}.`,
+            ButtonText: "Cancel Subscription",
+            SecondaryButtonText: "Never Mind",//for the modal
+            PrimaryButtonText: "Yes, Cancel Subscription",//for the modal
+            primaryOnClick: cancelSubscription,
+            secondaryOnClick: "close"
+        }
+    ]
+    
+    let SubscriptionData = [
+        {
+            ButtonText: "Add Subscription"
+            
+        }
+    ]
+    let cancelledSubscriptionData = [
+        {
+            title: "Cancellation Successful!",
+            subTitle: `Your subscription has been successfully cancelled. You can still access your workouts till ${impInfo?.redableNextPaymentDate}.`,
+            SecondaryButtonText: "Close",//for the modal
+            secondaryOnClick: "close"
+        }
+    ]
+
+    console.log("cancelled:",cancelled)
+    console.log("impInfo:",impInfo)
     if (data) {
         return (
             <>
@@ -190,9 +249,6 @@ export default function AccountDetailsComp(props) {
                                                Create Subscription
                                            </Button>
                                        </Stack> */}
-
-
-
                                 </Stack>
                                 <Stack direction="row" spacing={2} style={{ margin: "20px" }}>
                                     {impInfo?.merchantid &&
@@ -232,38 +288,60 @@ export default function AccountDetailsComp(props) {
 
                         <Grid size={6} style={{}}>
                             <Box style={{ width: "100%", boxShadow: "0 4px 5px 0 rgba(0,0,0,0.14),0 1px 10px 0 rgba(0,0,0,0.12),0 2px 4px -1px rgba(0,0,0,0.2),0 4px 5px 0 rgba(0,0,0,0.14)", padding: "20px" }}>
-                                <Typography id="modal-modal-title" variant="h4" component="h2">
-                                    Manage Subscription
-                                </Typography>
+                                <Stack direction="row" spacing={2} style={{ margin: "20px" }}>
+                                    <Stack direction="column" spacing={2} style={{ width: "100%" }}>
+                                        <Typography id="modal-modal-title" variant="h4" component="h2">
+                                            Manage Subscription
+                                        </Typography>
+                                    </Stack>
+                                    {impInfo?.status == "Active" &&
+                                        <Stack direction="column" spacing={2} style={{ width: "100%", display: "flex", alignItems: "flex-end" }}>
+                                            {/* <Button size='large' autoFocus variant='contained' onClick={cancelSubscription} >
+                                                Cancel Subscription
+                                            </Button> */}
+                                            <ModalPopUp data={cancelSubscriptionData} />
+                                        </Stack>
+                                    }
+                                    {impInfo?.status != "Active" &&
+                                        <Stack direction="column" spacing={2} style={{ width: "100%", display: "flex", alignItems: "flex-end" }}>
+                                            <Button size='large' autoFocus variant='contained' >
+                                                Add Subscription
+                                            </Button>
+                                            {/* <ModalPopUp data={SubscriptionData} /> */}
+                                        </Stack>
+                                    }
+                                    {cancelled &&
+                                        <ModalPopUp data={cancelledSubscriptionData} />
+                                    }
+                                </Stack>
                                 <Stack direction="row" spacing={2} style={{ margin: "20px" }}>
                                     {impInfo?.hasSubscription &&
                                         <>
                                             <Stack direction="column" spacing={2} style={{ justifyContent: "space-between", display: "flex" }}>
                                                 <Typography variant="h5" component="h2" sx={headlineFontSize}>Status</Typography>
+                                                {impInfo?.trial &&
+                                                    <Typography variant="h5" component="h2" sx={headlineFontSize}>Trial End Date</Typography>
+                                                }
                                                 <Typography variant="h5" component="h2" sx={headlineFontSize}>Name</Typography>
-                                                {/* <Typography variant="h5" component="h2" sx={headlineFontSize}>Current Plan</Typography> */}
                                                 <Typography variant="h5" component="h2" sx={headlineFontSize}>Amount</Typography>
+
                                                 <Typography variant="h5" component="h2" sx={headlineFontSize}>Next Payment Date</Typography>
                                                 <Typography variant="h5" component="h2" sx={headlineFontSize}>Payment Method</Typography>
                                             </Stack>
                                             <Stack direction="column" spacing={2} style={{ justifyContent: "space-between", display: "flex" }}>
-                                                <Typography variant="h5" component="h2" sx={valueFontSize}>{impInfo?.status}</Typography>
+                                                <Typography variant="h5" component="h2" sx={valueFontSize}>{impInfo?.status}{impInfo?.trial ? " (Trial Period)" : ""}</Typography>
+                                                {impInfo?.trial &&
+                                                    <Typography variant="h5" component="h2" sx={valueFontSize}>{impInfo?.trialEndDate}</Typography>
+                                                }
                                                 <Typography variant="h5" component="h2" sx={valueFontSize}>{impInfo?.subscriptionName}</Typography>
                                                 {/* <Typography variant="h5" component="h2" sx={valueFontSize}>{plan ?? "N/A"}</Typography> */}
                                                 <Typography variant="h5" component="h2" sx={valueFontSize}>${impInfo?.price} </Typography>
-                                                <Typography variant="h5" component="h2" sx={valueFontSize}>{impInfo?.nextPaymentDate}</Typography>
+                                                <Typography variant="h5" component="h2" sx={valueFontSize}>{impInfo?.redableNextPaymentDate}</Typography>
                                                 <Typography variant="h5" component="h2" sx={valueFontSize}>{`${cardType} ending in ${cardNumber}`}</Typography>
                                             </Stack>
                                         </>
                                     }
-                                    {!impInfo?.hasSubscription &&
-                                        <>
-                                            <Stack direction="column" spacing={2} style={{ justifyContent: "space-between", display: "flex" }}>
-                                                <Typography variant="h5" component="h2" sx={headlineFontSize}>No Subscription Found</Typography>
 
-                                            </Stack>
-                                        </>
-                                    }
 
                                 </Stack>
                             </Box>
