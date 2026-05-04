ddc4953a0da3f727619ba0f630fbd8ca672b7ff2
Prachi Chaudhary <admin@tlchatt.com>
2026-05-05 00:29:30 +0530

    account details page updated

--- app/accountDetails/page.js
+++ app/accountDetails/page.js
@@ -8,7 +8,7 @@ export default async function page({ searchParams }) {
     console.log("params:", params)
     
     const token = params.token
-    const userId = "ac83a88e-02a2-4be1-883f-9cc0f79d3c87"//params.userId
+    const userId = params.userId//params.userId
     // const username = params.username
     //0afff0c5-5e98-4932-9623-7d7978be6fcb    ---> non auth user with renewaldate in the past. 
     // stNvX7bF0ep2aUJTcGRdzJsXrlGuj7g2 ----->tlchatt auth user
