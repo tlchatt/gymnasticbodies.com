5d8f6aacfae42f97189cce4ff94df6f98cf5fa31
Greggory Wiley <admin@tlchatt.com>
2026-05-20 17:58:56 -0400

    paywall activated Renewal scripts

--- app/renew/page.js
+++ app/renew/page.js
@@ -2,17 +2,23 @@
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 import RenewalPortal from '@/components/RenewalPortal';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
-export default function RenewPage() {
+function RenewInner() {
     const searchParams = useSearchParams();
     const email = searchParams.get('email') ?? '';
+    return <RenewalPortal email={email} />;
+}
 
+export default function RenewPage() {
     return (
         <Elements stripe={stripePromise}>
-            <RenewalPortal email={email} />
+            <Suspense>
+                <RenewInner />
+            </Suspense>
         </Elements>
     );
 }
